c25368416a368dbc9545834219710c08aa09605e
Jayden Sky Liu <liuj0326216@gmail.com>
2026-06-29 11:53:08 -0700

    Edited Player code

--- js/player.js
+++ js/player.js
@@ -22,14 +22,14 @@ import { Images } from "./assets.js";
 import { SpriteAnimator, DIR } from "./sprite.js";
 import { Sound } from "./audio.js";
 
-const FRAMES = {idle: 5, run: 8, sword: 9};
+const FRAMES = {idle: 2, run: 4};
 
 export class Player{
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.width = 42;
-        this.height = 52;
+        this.width = 4;
+        this.height = 16;
         const spriteSize = CONFIG.PLAYER_FRAME_SIZE * CONFIG.SCALE;
         this.spriteOffsetX = (spriteSize - this.width) / 2;
         this.spriteOffsetY = 42;
@@ -84,34 +84,36 @@ export class Player{
         if(this.invincibleTimer > 0) this.invincibleTimer -= dt;
         if(this.justLeveledTimer > 0) this.justLeveledTimer -= dt;
 
-        if(this.attacking){
-            this.attackTimer -= dt;
-            this.anim.update(dt, FRAMES.sword);
-            if(this.attackTimer <= 0){
-                this.attacking = false;
-                this.anim.reset();
-            }
-            return;
-        }
+        // if(this.attacking){
+        //     this.attackTimer -= dt;
+        //     this.anim.update(dt, FRAMES.sword);
+        //     if(this.attackTimer <= 0){
+        //         this.attacking = false;
+        //         this.anim.reset();
+        //     }
+        //     return;
+        // }
 
-        if(Input.wasPressed("Space")){
-            this.startAttack();
-            return;
-        }
+        // if(Input.wasPressed("Space")){
+        //     this.startAttack();
+        //     return;
+        // }
 
         let dx = 0, dy = 0;
+        let speed = CONFIG.PLAYER_SPEED;
         if(Input.left){ dx -= 1; this.dir = DIR.LEFT;}
         if(Input.right){ dx += 1; this.dir = DIR.RIGHT;}
         if(Input.up){ dy -= 1; this.dir = DIR.UP;}
         if(Input.down){ dy += 1; this.dir = DIR.DOWN;}
+        if(Input.shift){ speed = 100; console.log("Speeding up!"); }else{ speed = CONFIG.PLAYER_SPEED; }
 
         this.moving = (dx !== 0 || dy !== 0);
         if(this.moving){
             const len = Math.hypot(dx, dy);
             dx /= len;
             dy /= len;
-            const stepX = dx * CONFIG.PLAYER_SPEED * dt;
-            const stepY = dy * CONFIG.PLAYER_SPEED * dt;
+            const stepX = dx * speed * dt;
+            const stepY = dy * speed * dt;
             this.moveAxis(stepX, 0, map);
             this.moveAxis(0, stepY, map);
             this.anim.update(dt, FRAMES.run);
@@ -122,7 +124,7 @@ export class Player{
     moveAxis(mx, my, map){
         const nextX = this.x + mx;
         const nextY = this.y + my;
-        const corners = [[nextX, nextY], [nextX + this.width - 1, nextY], [nextX, nextY + this.height - 1], [nextX + this.width - 1, nextY + this.height - 1]];
+        const corners = [[nextX, nextY], [nextX + this.width - 0.1, nextY], [nextX, nextY + this.height - 0.1], [nextX + this.width - 0.1, nextY + this.height - 0.1]];
         for(const[cx, cy] of corners){
             if(map.isSolidAtPixel(cx, cy)){
                 return;
@@ -132,32 +134,32 @@ export class Player{
         this.y = nextY;
     }
 
-    startAttack(){
-        this.attacking = true;
-        this.attackTimer = FRAMES.sword / CONFIG.ANIM_FPS;
-        this.attackHasHit = false;
-        this.anim.reset();
-        Sound.play("attack");
-    }
+    // startAttack(){
+    //     this.attacking = true;
+    //     this.attackTimer = FRAMES.sword / CONFIG.ANIM_FPS;
+    //     this.attackHasHit = false;
+    //     this.anim.reset();
+    //     Sound.play("attack");
+    // }
 
-    getAttackPoint(){
-        const cx = this.x + this.width / 2;
-        const cy = this.y + this.height / 2;
-        const r = CONFIG.PLAYER_ATTACK_RANGE;
-        if(this.dir === DIR.LEFT) return { x: cx - r, y: cy };
-        if(this.dir === DIR.RIGHT) return { x: cx + r, y: cy };
-        if(this.dir === DIR.UP) return { x: cx, y: cy - r };
-        return { x: cx, y: cy + r };
-    }
+    // getAttackPoint(){
+    //     const cx = this.x + this.width / 2;
+    //     const cy = this.y + this.height / 2;
+    //     const r = CONFIG.PLAYER_ATTACK_RANGE;
+    //     if(this.dir === DIR.LEFT) return { x: cx - r, y: cy };
+    //     if(this.dir === DIR.RIGHT) return { x: cx + r, y: cy };
+    //     if(this.dir === DIR.UP) return { x: cx, y: cy - r };
+    //     return { x: cx, y: cy + r };
+    // }
 
-    takeDamage(amount){
-        if(this.invincibleTimer > 0) return;
-        this.hp = Math.max(0, this.hp - amount);
-        this.invincibleTimer = 0.8;
-        Sound.play("hit");
-    }
+    // takeDamage(amount){
+    //     if(this.invincibleTimer > 0) return;
+    //     this.hp = Math.max(0, this.hp - amount);
+    //     this.invincibleTimer = 0.8;
+    //     Sound.play("hit");
+    // }
 
-    get isDead(){ return this.hp <= 0; }
+    // get isDead(){ return this.hp <= 0; }
 
     draw(ctx, camera){
         if(this.invincibleTimer > 0 && Math.floor(this.invincibleTimer * 12) % 2 === 0){
